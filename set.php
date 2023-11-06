af930d9740dd296d6134a2f81fb06797fb325638
Irfan8612 <149646353+Irfan8612@users.noreply.github.com>
2023-11-06 14:27:31 +0700

    Update set.php

--- set.php
+++ set.php
@@ -1,42 +1,21 @@
 <?php
-
-/**
- * This file is part of the PHP Telegram Bot example-bot package.
- * https://github.com/php-telegram-bot/example-bot/
- *
- * (c) PHP Telegram Bot Team
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/**
- * This file is used to set the webhook.
- */
-
 // Load composer
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-// Load all configuration options
-/** @var array $config */
-$config = require __DIR__ . '/config.php';
+$bot_api_key  = '6579229262:AAEyEKuyxJLXf6utQMeUQ5OE9lqmjjn4LGc';
+$bot_username = '@Cobasaja134_bot';
+$hook_url     = 'https://your-domain/path/to/hook.php';
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($config['api_key'], $config['bot_username']);
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    /**
-     * REMEMBER to define the URL to your hook.php file in:
-     * config.php: ['webhook']['url'] => 'https://your-domain/path/to/hook.php'
-     */
-
-    // Set the webhook
-    $result = $telegram->setWebhook($config['webhook']['url']);
-
-    // To use a self-signed certificate, use this line instead
-    // $result = $telegram->setWebhook($config['webhook']['url'], ['certificate' => $config['webhook']['certificate']]);
-
-    echo $result->getDescription();
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    echo $e->getMessage();
+    // log telegram errors
+    // echo $e->getMessage();
 }
